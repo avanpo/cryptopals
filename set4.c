b8620107c9952a39e2aaf3d3d8e0c8d13c2dccb6
Alex van Poppelen <avanpoppelen@gmail.com>
2016-03-21 17:34:09 +0100

    challenge 26

--- set4.c
+++ set4.c
@@ -58,8 +58,77 @@ void challenge_25()
 	print_str((char *)pt);
 }
 
+int source_26(unsigned char *in, size_t length, unsigned char *out)
+{
+	char prepend[] = "comment1=cooking%20MCs;userdata=";
+	char append[] = ";comment2=%20like%20a%20pound%20of%20bacon";
+	int pre_length = strlen(prepend), ap_length = strlen(append);
+
+	char *encoding = malloc((pre_length + 3 * length + ap_length) * sizeof(char));
+
+	memcpy(encoding, prepend, pre_length);
+	int i, j;
+	for (i = 0, j = pre_length; i < length; ++i, ++j) {
+		if (in[i] == ';' || in[i] == '=') {
+			encoding[j++] = '"';
+			encoding[j++] = in[i];
+			encoding[j] = '"';
+		} else {
+			encoding[j] = in[i];
+		}
+	}
+	memcpy(encoding + j, append, ap_length);
+
+	unsigned char nonce[8] = {0};
+	int encrypted_length = AES_CTR((unsigned char *)encoding, out,
+			j + ap_length, get_static_key(), nonce);
+
+	free(encoding);
+	return encrypted_length;
+}
+
+int receiver_26(unsigned char *in, size_t length)
+{
+	unsigned char *out = malloc(length * sizeof(unsigned char));
+
+	unsigned char nonce[8] = {0};
+	int str_length = AES_CTR(in, out, length, get_static_key(), nonce);
+
+	unsigned char target[13] = ";admin=true;";
+
+	int i, j;
+	for (i = 0, j = 0; i < str_length; ++i) {
+		if (out[i] == target[j]) {
+			++j;
+			if (j > 11) {
+				free(out);
+				return 1;
+			}
+		} else {
+			j = 0;
+		}
+	}
+
+	free(out);
+	return 0;
+}
+
 void challenge_26()
 {
+	unsigned char input[11] = ":admin<true";
+	unsigned char output[512] = {0};
+
+	int len = source_26(input, 11, output);
+
+	output[32] ^= 0x01;
+	output[38] ^= 0x01;
+
+	int result = receiver_26(output, len);
+	if (result) {
+		print_str("admin priveleges");
+	} else {
+		print_str("stay back, plebeian");
+	}
 }
 
 int main(int argc, char *argv[])
